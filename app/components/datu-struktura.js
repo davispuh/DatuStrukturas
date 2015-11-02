c6703208a9f7bfcd2759dc59bcd6ed012370e28d
Dāvis <davispuh@gmail.com>
2015-11-02 06:39:42 +0200

    Pievienot fabric.js un uzzīmēt elementus

--- app/components/datu-struktura.js
+++ app/components/datu-struktura.js
@@ -6,8 +6,10 @@ export default Ember.Component.extend({
         Ember.run.scheduleOnce('afterRender', this, this.afterRenderEvent);
     },
     afterRenderEvent : function(){
+        this.set('attels', new fabric.Canvas('attels'));
         this.get('controller').send('izveidot');
     },
+    attels: null,
     elementi: 6,
     masivs: [],
     pozicijas: ['Sākumā', 'Beigās'],
@@ -22,9 +24,33 @@ export default Ember.Component.extend({
         izveidot() {
             this.masivs = [];
             for (var i=0; i < this.elementi; i++) {
-                this.masivs.push(Math.round(Math.random() * this.elementi * 10));
+                this.masivs.push(Math.round(Math.random() * 100));
             }
-            var attels = this.$(this.element).find('#attels');
+            this.attels.clear();
+            var elements = new fabric.Rect({
+                left: 50,
+                top: 300,
+                width: 70,
+                height: 70,
+                fill: 'white',
+                strokeWidth: 4,
+                stroke: 'blue',
+                selectable: false
+            });
+            var teksts = new fabric.Text('', {
+                top: 300 + (elements.height / 4),
+                fill: 'black',
+                selectable: false
+            });
+            for (var i=0; i < this.elementi; i++) {
+                elements.left = 50 + (elements.width + 20) * i;
+                teksts.setText(this.masivs[i].toString());
+                teksts.left = elements.left + (elements.width / 4);
+                this.attels.add(elements);
+                this.attels.add(teksts);
+                elements = fabric.util.object.clone(elements);
+                teksts = fabric.util.object.clone(teksts);
+            }
         },
         pievienot() {
         },
